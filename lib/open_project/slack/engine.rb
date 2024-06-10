3eb31fcaf4b84dea19095a28ef817a391abb0a3f
Markus Kahl <machisuji@gmail.com>
2024-06-10 13:10:31 +0100

    replace old slack icon with new octicon

--- lib/open_project/slack/engine.rb
+++ lib/open_project/slack/engine.rb
@@ -24,7 +24,7 @@ module OpenProject::Slack
            :slack_settings,
            { controller: '/admin/settings', action: :show_plugin, id: :openproject_slack },
            caption: :label_slack_plugin,
-           icon: 'slack',
+           icon: 'op-logo-slack',
            if: ->(*) { User.current.admin? && ::OpenProject::Slack.enabled? }
     end
 
